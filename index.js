fdfb11a7d3a7cc48cf16caacc967ac5bfc5b44fe
github username <github email address>
2019-01-28 16:15:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file
+function findMatching (collection, name) {
+  matches = []
+  for (const element of collection) {
+    if ( element.toLowerCase() === name.toLowerCase() ) {
+      matches.push(element)
+    }
+  }
+  return matches
+}
+
+function fuzzyMatch (collection, name) {
+  matches = []
+  for (const element of collection) {
+    if (element.slice(0,2) === name.slice(0,2)){
+      matches.push(element)
+    }
+  }
+  return matches
+}
+// function colorFilter (collection, color) {
+//   for (const user of collection) {
+//     if (user.favoriteColor === color) {
+//       console.log(user.firstName);
+//     }
+//   }
+// }
+function matchName (collection, name) {
+  matches = []
+  for (const object of collection) {
+    if (object.name === name) {
+      matches.push(object)
+    }
+  }
+  return matches
+}
